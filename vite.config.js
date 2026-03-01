e9d4471b48ba77fc27686a06d311848bd83a97b6
kingjudecole <kingjudecole@gmail.com>
2026-03-01 08:29:47 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/wateredgardenchurch/',
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
   },
+  base: '/wateredgardenchurch/'
 })
